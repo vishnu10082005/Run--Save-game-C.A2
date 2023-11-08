6408ee100295f7cf67594f2598ec01fc71344f4c
vishnu10082005 <vishnu.preetham@kalvium.community>
2023-11-08 16:24:03 +0530

    complete game2

--- gameover.js
+++ gameover.js
@@ -5,3 +5,11 @@ bgmMusic.play();
 let scoreUpdate = localStorage.getItem('scoreCont');
 let score = document.getElementById('score');
 score.innerHTML = `Your score is: ${scoreUpdate}`;
+    // Retrieve the data from local storage
+    const playerData = JSON.parse(localStorage.getItem("playerData"));
+
+    // Display the data on the second page
+    if (playerData) {
+        document.getElementById("nameOutput").textContent = playerData.name;
+        document.getElementById("ageOutput").textContent = playerData.age;
+        document.getElementById("squadOutput").textContent = playerData.squad;}
--- playpage/play.js
+++ playpage/play.js
@@ -2,3 +2,26 @@
 let bgmMusic = new Audio("./assests/bgm1.mp3");
 bgmMusic.play();
 loop = true;
+  // Get references to the input fields and the "Save Data" button
+  const nameInput = document.getElementById("nameInput");
+  const ageInput = document.getElementById("ageInput");
+  const squadInput = document.getElementById("squadInput");
+  const saveButton = document.getElementById("saveData");
+
+  // Event listener for the button click
+  saveButton.addEventListener("click", () => {
+      // Get values from input fields
+      const name = nameInput.value;
+      const age = ageInput.value;
+      const squad = squadInput.value;
+
+      // Create an object to store the data
+      const data = {
+          name,
+          age,
+          squad,
+      };
+
+      // Store the data in local storage as a JSON string
+      localStorage.setItem("playerData", JSON.stringify(data));
+  });
